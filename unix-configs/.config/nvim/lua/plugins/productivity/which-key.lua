f2a346801f413113aab067efbb34271b9cce0827
Paul DiLoreto <paul.diloreto@gmail.com>
2025-08-29 15:10:58 -0700

    feat(nvim): enable relative line numbers

--- unix-configs/.config/nvim/lua/plugins/productivity/which-key.lua
+++ unix-configs/.config/nvim/lua/plugins/productivity/which-key.lua
@@ -36,13 +36,13 @@ return {
       wk.setup(opts)
       -- Native neovim actions
       wk.add({
-        -- {
-        --   "<leader>r",
-        --   ":exec &rnu == 1 ? 'set nornu!' : 'set rnu!'<CR>",
-        --   desc = "Toggle rel num",
-        --   silent = true,
-        --   mode = { "n", "v" },
-        -- },
+        {
+          "<leader>r",
+          ":exec &rnu == 1 ? 'set nornu!' : 'set rnu!'<CR>",
+          desc = "Toggle rel num",
+          silent = true,
+          mode = { "n", "v" },
+        },
         {
           "<leader>S",
           ":sort<CR>",
