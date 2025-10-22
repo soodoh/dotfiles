fafc8976f426a514749ef829c87a9f0c0070f8ee
Paul DiLoreto <soodohh@pm.me>
2025-10-22 16:22:41 -0700

    feat(nvim): make mcp-hub bundled to avoid global

--- unix-configs/.config/nvim/lua/plugins/productivity/avante.lua
+++ unix-configs/.config/nvim/lua/plugins/productivity/avante.lua
@@ -4,9 +4,10 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    build = "bundled_build.lua", -- Bundles `mcp-hub` binary along with the neovim plugin
     config = function()
       require("mcphub").setup({
+        use_bundled_binary = true,
         config = vim.fn.expand("~/.config/mcphub/servers.json"),
       })
 
