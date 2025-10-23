e8202735700761c20e06f8d2ab4be96ea585f845
Paul DiLoreto <paul.diloreto@gmail.com>
2025-10-22 18:28:07 -0700

    feat: use tokyonight theme w/ transparency

--- unix-configs/.config/nvim/lua/plugins/ui/lualine.lua
+++ unix-configs/.config/nvim/lua/plugins/ui/lualine.lua
@@ -3,34 +3,9 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      local colors = {
-        blue = "#80a0ff",
-        cyan = "#79dac8",
-        white = "#c6c6c6",
-        red = "#ff5189",
-        purple = "#d183e8",
-        grey = "#303030",
-      }
-
-      local bubbles_theme = {
-        normal = {
-          a = { fg = "black", bg = colors.purple },
-          b = { fg = colors.white, bg = colors.grey },
-          c = { fg = colors.white },
-        },
-        insert = { a = { fg = "black", bg = colors.blue } },
-        visual = { a = { fg = "black", bg = colors.cyan } },
-        replace = { a = { fg = "black", bg = colors.red } },
-        inactive = {
-          a = { fg = colors.white, bg = "black" },
-          b = { fg = colors.white, bg = "black" },
-          c = { fg = colors.white },
-        },
-      }
-
       require("lualine").setup({
         options = {
-          theme = bubbles_theme,
+          theme = "tokyonight",
           component_separators = "",
           section_separators = { left = "", right = "" },
         },
--- unix-configs/.config/nvim/lua/plugins/ui/theme.lua
+++ unix-configs/.config/nvim/lua/plugins/ui/theme.lua
@@ -1,41 +1,23 @@
 return {
   {
-    "erl-koenig/theme-hub.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      -- Optional: Telescope integration
-      "nvim-telescope/telescope-ui-select.nvim",
-      -- Optional: for themes that use lush (will be notified if a theme requires it)
-      "rktjmp/lush.nvim",
-    },
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
     config = function()
-      require("theme-hub").setup({
-        auto_install_on_select = true,
-        apply_after_install = true,
-        persistent = true,
-      })
-      require("telescope").load_extension("ui-select")
-      require("which-key").add({
-        { "<leader>t", group = "Themes" },
-        {
-          "<leader>th",
-          ":ThemeHub<CR>",
-          desc = "Switch/Preview themes",
-        },
-        {
-          "<leader>tt",
-          function()
-            require("telescope.builtin").colorscheme({
-              enable_preview = true,
-              ignore_builtins = true,
-            })
-          end,
-          desc = "Switch/Preview themes",
+      require("tokyonight").setup({
+        style = "night",
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = { italic = true },
+          variables = {},
+          sidebars = "transparent",
+          floats = "transparent",
         },
       })
-
-      -- Set default colorscheme, so persisted theme doesnt flash on load
-      vim.cmd("colorscheme tokyodark")
+      vim.cmd.colorscheme("tokyonight-night")
     end,
   },
 }
