d6c16e2e46cf1b7a296646526af665d2b172fc90
Paul DiLoreto <paul.diloreto@gmail.com>
2025-10-23 12:45:19 -0700

    refactor(wezterm): move config files

--- unix-configs/.config/wezterm/keybindings/init.lua
+++
@@ -1,9 +0,0 @@
--- Combine keybinding modules here
-return {
-	apply_to_config = function(config)
-		local panes_tabs = require("keybindings.panes_tabs")
-		panes_tabs.apply_to_config(config)
-		local common = require("keybindings.common")
-		common.apply_to_config(config)
-	end,
-}
--- unix-configs/.config/wezterm/wezterm.lua
+++ unix-configs/.config/wezterm/wezterm.lua
@@ -4,9 +4,12 @@ local config = wezterm.config_builder()
 config.keys = {}
 
 -- Split up settings into relevant modules
-local keybindings = require("keybindings")
-keybindings.apply_to_config(config)
-
+local common = require("common")
+common.apply_to_config(config)
+local domains_workspaces = require("domains_workspaces")
+domains_workspaces.apply_to_config(config)
+local panes_tabs = require("panes_tabs")
+panes_tabs.apply_to_config(config)
 local ui = require("ui")
 ui.apply_to_config(config)
 
