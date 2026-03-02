081dbec3ebc480ac78c6bff783af8c4c5f4d534f
Paul DiLoreto <soodohh@pm.me>
2026-03-02 14:35:45 -0800

    feat(fish): better tmux plugin

--- unix-configs/.config/fish/custom/conf.d/tmux.fish
+++ unix-configs/.config/fish/custom/conf.d/tmux.fish
@@ -1,11 +1,3 @@
-# Auto-start tmux on interactive shell (replaces omz tmux plugin)
-if status is-interactive
-    and command -q tmux
-    and not set -q TMUX
-    and not set -q VSCODE_RESOLVING_ENVIRONMENT
-    tmux new-session -A -s main
-end
-
 # TPM (Tmux Plugin Manager) - auto install if needed
 set -l tpm_dir "$HOME/.config/tmux/plugins/tpm"
 if not test -d $tpm_dir
