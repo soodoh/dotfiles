441eb8c94216800f9e9dbd8387472cedff074a49
Paul DiLoreto <soodohh@pm.me>
2026-06-29 08:57:35 -0700

    feat(shell): atuin up arrow

--- unix-configs/.config/fish/custom/conf.d/atuin.fish
+++ unix-configs/.config/fish/custom/conf.d/atuin.fish
@@ -1,5 +1,6 @@
 # Atuin shell history
 # Replaces Sponge for cleaner, tmux-safe history tracking.
 if status is-interactive; and command -q atuin
-    atuin init fish | source
+    # Keep Ctrl-R for Atuin's UI, but leave Up Arrow on fish's normal history recall.
+    atuin init fish --disable-up-arrow | source
 end
