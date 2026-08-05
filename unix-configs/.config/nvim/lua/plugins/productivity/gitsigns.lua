03cd5e45f0eb79f6504eb8147914d4abd0d95167
Paul DiLoreto <soodohh@pm.me>
2026-08-05 16:12:28 -0700

    feat(nvim): disable gitsigns watch dir

--- unix-configs/.config/nvim/lua/plugins/productivity/gitsigns.lua
+++ unix-configs/.config/nvim/lua/plugins/productivity/gitsigns.lua
@@ -15,6 +15,7 @@ return {
           untracked = { text = "┆" },
         },
         signcolumn = true,
+        watch_gitdir = { enable = false },
         current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
         current_line_blame_opts = {
           virt_text = true,
