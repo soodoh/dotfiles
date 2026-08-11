86b53e022a8297f1a523915bd88387a7252c5db6
Paul DiLoreto <soodohh@pm.me>
2026-08-11 16:02:45 -0700

    fix(mac): ensure window tools start reliably

--- nix/modules/darwin/window-management.nix
+++ nix/modules/darwin/window-management.nix
@@ -1,8 +1,18 @@
 { host, pkgs, ... }:
+let
+  windowManagementPath = "/etc/profiles/per-user/${host.username}/bin:/run/current-system/sw/bin:/usr/bin:/bin:/usr/sbin:/sbin";
+  sketchybarAfterAeroSpace = pkgs.writeShellScript "sketchybar-after-aerospace" ''
+    until ${pkgs.aerospace}/bin/aerospace list-workspaces --all >/dev/null 2>&1; do
+      /bin/sleep 1
+    done
+    exec ${pkgs.sketchybar}/bin/sketchybar
+  '';
+in
 {
   launchd.user.agents.sketchybar = {
     serviceConfig = {
-      ProgramArguments = [ "${pkgs.sketchybar}/bin/sketchybar" ];
+      ProgramArguments = [ "${sketchybarAfterAeroSpace}" ];
+      EnvironmentVariables.PATH = windowManagementPath;
       RunAtLoad = true;
       KeepAlive = true;
       ProcessType = "Interactive";
@@ -19,6 +29,7 @@
         "inactive_color=0xff3b4261"
         "width=5.0"
       ];
+      EnvironmentVariables.PATH = windowManagementPath;
       RunAtLoad = true;
       KeepAlive = true;
       ProcessType = "Interactive";
