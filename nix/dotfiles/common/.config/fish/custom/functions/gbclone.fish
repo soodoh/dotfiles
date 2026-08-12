83b53dad3cd156a6dff0e3623c4649274cb7f6d6
Paul DiLoreto <soodohh@pm.me>
2026-08-12 14:57:56 -0700

    fix(shell): configure remote tracking in gbclone

--- nix/dotfiles/common/.config/fish/custom/functions/gbclone.fish
+++ nix/dotfiles/common/.config/fish/custom/functions/gbclone.fish
@@ -16,7 +16,10 @@ function gbclone -d "clone a repo into a bare .git dir with a default-branch wor
   end
 
   command git clone --bare "$repo_url" "$repo_name/.git"; or return
+  command git --git-dir="$repo_name/.git" config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'; or return
+  command git --git-dir="$repo_name/.git" fetch origin; or return
 
   set -l default_branch (path basename (command git --git-dir="$repo_name/.git" symbolic-ref HEAD)); or return
+  command git --git-dir="$repo_name/.git" branch --set-upstream-to="origin/$default_branch" "$default_branch"; or return
   command git --git-dir="$repo_name/.git" worktree add "$repo_name/$default_branch" "$default_branch"
 end
