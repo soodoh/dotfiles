46eb8ab1d9e3f90331da1e6ac690905901c61639
Paul DiLoreto <soodohh@pm.me>
2026-08-17 13:33:40 -0700

    fix(nvim): lazy.nvim self management

--- dotfiles/common/nvim/lua/lazy-bootstrap.lua
+++ dotfiles/common/nvim/lua/lazy-bootstrap.lua
@@ -1,5 +1,4 @@
 local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-local lazy_commit = "26d121ea13bee96b079403cee6598f04969d4983"
 
 if not vim.uv.fs_stat(lazy_path) then
   local output = vim.fn.system({
@@ -11,18 +10,6 @@ if not vim.uv.fs_stat(lazy_path) then
   })
   if vim.v.shell_error ~= 0 then
     error("failed to clone lazy.nvim:\n" .. output)
-  end
-end
-
-local current_commit = vim.fn.system({ "git", "-C", lazy_path, "rev-parse", "HEAD" }):gsub("%s+$", "")
-if current_commit ~= lazy_commit then
-  local output = vim.fn.system({ "git", "-C", lazy_path, "fetch", "origin", lazy_commit })
-  if vim.v.shell_error ~= 0 then
-    error("failed to fetch pinned lazy.nvim commit:\n" .. output)
-  end
-  output = vim.fn.system({ "git", "-C", lazy_path, "checkout", "--detach", lazy_commit })
-  if vim.v.shell_error ~= 0 then
-    error("failed to check out pinned lazy.nvim commit:\n" .. output)
   end
 end
 
